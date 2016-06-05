9297a24bb5ab90f3ae7abd4501c60af80bf74c62
cadywsq <cadywsq@gmail.com>
2016-06-05 13:27:39 -0400

    128. Longest Consecutive Sequence@Cady

---
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int maxLen = 1;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                continue;
+            }
+            else if (nums[i] == nums[i-1]+1) {
+                count++;
+            } else {
+                maxLen = Math.max(maxLen, count);
+                count = 1;
+            }
+        }
+        return Math.max(maxLen, count);
+    }
+}

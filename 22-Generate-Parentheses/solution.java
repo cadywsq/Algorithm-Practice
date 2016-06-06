c3794585d64224c554a2281d32008fc4efc7854c
cadywsq <cadywsq@gmail.com>
2016-06-05 21:39:16 -0400

    22. Generate Parentheses@Cady

---
+++ 22-Generate-Parentheses/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        if (n <= 0) {
+            return result;
+        }
+        helper(n, 0, 0, "", result);
+        return result;
+    }
+    
+    private void helper(int n, int openCount, int closeCount, String item, List<String> result) {
+        if (item.length() == n*2) {
+            result.add(item);
+            return;
+        }
+        
+        if (openCount == closeCount) {
+            helper(n, openCount+1, closeCount, item + "(", result);
+            return;
+        }
+        
+        if (openCount < n) {
+            helper(n, openCount+1, closeCount, item + "(", result);
+        }
+        if (closeCount < n) {
+            helper(n, openCount, closeCount+1, item + ")", result);
+        }
+    }
+}

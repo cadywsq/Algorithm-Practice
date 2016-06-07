e6ab20f495a8de21fe4d01ad388a87a5a92c20ba
cadywsq <cadywsq@gmail.com>
2016-06-07 00:40:09 -0400

    131. Palindrome Partitioning@Cady

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -1,14 +1,22 @@
 public class Solution {
     public List<List<String>> partition(String s) {
+        return dfsSolution(s);
+    }
+    
+    // DFS solution: partition the string to check whether first part is palindrome, then recursively partition the second part.
+    // Time complexity: O(2^N), as there are 2^N partitions.
+    private List<List<String>> dfsSolution(String s) {
         List<List<String>> result = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return result;
         }
-        partitionHelper(s, new ArrayList<>(), result, 0);
+        // record whether s.substring(i,j) is palindrome 
+        boolean[][] dp = new boolean[s.length()][s.length()+1];
+        partitionHelper(s, dp, new ArrayList<>(), result, 0);
         return result;
     }
     
-    private void partitionHelper(String s, List<String> element, List<List<String>> result, int index) {
+    private void partitionHelper(String s, boolean[][] dp, List<String> element, List<List<String>> result, int index) {
         if (index == s.length()) {
             result.add(new ArrayList<>(element));
             return;
@@ -16,10 +24,11 @@ public class Solution {
         
         for (int i = index+1; i <= s.length(); i++) {
             String str = s.substring(index, i);
-            if (isPalindrome(str)) {
+            if (dp[index][i] || isPalindrome(str)) {
+                dp[index][i] = true;
                 element.add(str);
                 // if current s substring is palindrome, recursively check the rest of s.
-                partitionHelper(s, element, result, i);
+                partitionHelper(s, dp, element, result, i);
                 element.remove(element.size()-1);
             }
         }

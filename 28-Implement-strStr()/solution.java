d9863ca6549291de0c92faabd0c6b97dc295b204
cadywsq <cadywsq@gmail.com>
2016-06-08 18:00:19 -0400

    28. Implement strStr()@Cady

---
+++ 28-Implement-strStr()/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        
+        for (int i = 0; i <= haystack.length()-needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                int j = 0;
+                for (; j < needle.length(); j++) {
+                    if (haystack.charAt(i+j) != needle.charAt(j)) {
+                        break;
+                    }
+                }
+                if (j == needle.length()) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}

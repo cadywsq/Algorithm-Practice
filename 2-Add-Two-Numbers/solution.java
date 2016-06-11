4492e7a1c179961ec5b15d0cca18d3c566db6b8c
cadywsq <cadywsq@gmail.com>
2016-06-11 11:54:32 -0400

    2. Add Two Numbers@Cady

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -8,29 +8,27 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-        if (l1 == null || l2 == null) {
-            return l1==null ? l2 : l1;
-        }
-        
+        // no need to do null-check for l1, l2.
         ListNode sentinel = new ListNode(-1);
         ListNode cur = sentinel;
+        // create extra pointer to avoid p1, p2 referencing object being changed.
+        ListNode p1 = l1;
+        ListNode p2 = l2;
         int sum = 0;
-        while (l1 != null || l2 != null) {
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
             }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
             }
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
             sum = sum / 10;
         }
+        // check whether there is carry left after complete traversal.
         if (sum == 1) {
             cur.next = new ListNode(1);
         }

f38085d1102a0fa7b9e7d515007ca548f05ae09b
cadywsq <cadywsq@gmail.com>
2016-06-11 16:44:49 -0400

    206. Reverse Linked List@Cady

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}

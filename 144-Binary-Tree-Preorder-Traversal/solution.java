0faaa6efd1789ce506b067d6742035059e5b0e68
Cady Wang <cadywsq@gmail.com>
2016-06-12 11:21:44 -0400

    144. Binary Tree Preorder Traversal@Cady

---
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // be aware recursive preorder traversal is tail recursion, it's different from inorder.
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            // add root to the list
+            TreeNode cur = stack.pop();
+            result.add(cur.val);
+            
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+        return result;
+    }
+}

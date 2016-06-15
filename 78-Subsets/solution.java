c1295407fb8ef80dcc381e5bfca191667346564a
Cady Wang <cadywsq@gmail.com>
2016-06-15 17:09:23 -0400

    78. Subsets@Cady

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -4,21 +4,36 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return result;
         }
-        getSubsets(nums, 0, new ArrayList<>(), result);
+        getSubsets2(nums, 0, new ArrayList<>(), result);
         return result;
     }
     
+    /**Solution1: common dfs.
+     */
     private void getSubsets(int[] nums, int start, List<Integer> ans, List<List<Integer>> result) {
         result.add(new ArrayList<>(ans));
-        
         if (start == nums.length) {
             return;
         }
-        
         for (int i = start; i < nums.length; i++) {
             ans.add(nums[i]);
             getSubsets(nums, i+1, ans, result);
             ans.remove(ans.size()-1);
         }
     }
+    
+    /**Solution2: for each element, use or not use. (coin change)
+     */
+     private void getSubsets2(int[] nums, int index, List<Integer> ans, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(ans));
+            return;
+        }
+        
+        ans.add(nums[index]);
+        getSubsets(nums, index+1, ans, result);
+        ans.remove(ans.size()-1);
+        
+        getSubsets(nums, index+1, ans, result);
+     }
 }

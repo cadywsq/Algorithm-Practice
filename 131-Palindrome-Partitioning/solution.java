98459510a39a24321335668905d2b7af6602649c
Cady Wang <cadywsq@gmail.com>
2016-06-15 21:12:19 -0400

    131. Palindrome Partitioning@Cady

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -1,44 +1,37 @@
 public class Solution {
     public List<List<String>> partition(String s) {
-        return dfsSolution(s);
-    }
-    
-    // DFS solution: partition the string to check whether first part is palindrome, then recursively partition the second part.
-    // Time complexity: O(2^N), as there are 2^N partitions.
-    private List<List<String>> dfsSolution(String s) {
         List<List<String>> result = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return result;
         }
-        // record whether s.substring(i,j) is palindrome 
-        boolean[][] dp = new boolean[s.length()][s.length()+1];
-        partitionHelper(s, dp, new ArrayList<>(), result, 0);
+        // cache[i][j] represents whether s.substring(i,j) is palindrome.
+        boolean[][] cache = new boolean[s.length()][s.length()+1];
+        partition(s, 0, new ArrayList<>(), result, cache);
         return result;
     }
     
-    private void partitionHelper(String s, boolean[][] dp, List<String> element, List<List<String>> result, int index) {
+    private void partition(String s, int index, List<String> part, List<List<String>> result, boolean[][] cache) {
         if (index == s.length()) {
-            result.add(new ArrayList<>(element));
+            result.add(new ArrayList<>(part));
             return;
         }
-        
+        // i represents the end of the substring.
         for (int i = index+1; i <= s.length(); i++) {
-            String str = s.substring(index, i);
-            if (dp[index][i] || isPalindrome(str)) {
-                dp[index][i] = true;
-                element.add(str);
-                // if current s substring is palindrome, recursively check the rest of s.
-                partitionHelper(s, dp, element, result, i);
-                element.remove(element.size()-1);
+            String substring = s.substring(index, i);
+            if (cache[index][i] || isPalindrome(substring)) {
+                cache[index][i] = true;
+                part.add(substring);
+                // for recursion call, index change to i as substring starts from index+1.
+                partition(s, i, part, result, cache);
+                part.remove(part.size()-1);
             }
         }
     }
     
     private boolean isPalindrome(String s) {
-        int len = s.length();
         char[] chars = s.toCharArray();
-        for (int i = 0; i <= len/2; i++) {
-            if (chars[i] != chars[len-1-i]) {
+        for (int i = 0; i < chars.length/2; i++) {
+            if (chars[i] != chars[chars.length-1-i]) {
                 return false;
             }
         }

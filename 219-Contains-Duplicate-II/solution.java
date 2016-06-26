a4736e4aace958b4c9b9b35bdf4bf8c0934df34c
Cady Wang <cadywsq@gmail.com>
2016-06-26 17:37:08 -0400

    219. Contains Duplicate II@Cady

---
+++ 219-Contains-Duplicate-II/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // key: array element; value: array index
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(map.get(nums[i])-i) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}

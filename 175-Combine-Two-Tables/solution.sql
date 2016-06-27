65d0b6c9e63a494ff9507c6d212821e23a84ec9b
Cady Wang <cadywsq@gmail.com>
2016-06-27 14:06:57 -0400

    175. Combine Two Tables@Cady

--- 175-Combine-Two-Tables/solution.sql
+++ 175-Combine-Two-Tables/solution.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
--- SELECT Firstname, Lastname, City, State 
--- FROM Person LEFT JOIN Address 
--- ON Person.PersonId = Address.PersonId;
+
 select FirstName, LastName, City, State 
 from Person left join Address 
 on Person.PersonId = Address.PersonId;

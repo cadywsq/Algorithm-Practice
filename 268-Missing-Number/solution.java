a7c522c38911cc4235e20fe21443d1a9b76f494f
Cady Wang <cadywsq@gmail.com>
2016-07-19 11:33:13 -0400

    268. Missing Number@Cady

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,5 +1,10 @@
 public class Solution {
     public int missingNumber(int[] nums) {
+        return solution2(nums);
+    }
+    
+    // Math method.
+    private int solution1(int[] nums) {
         int n = nums.length;
         int total = n * (n+1)/2;
         int sum = 0;
@@ -7,5 +12,15 @@ public class Solution {
             sum += nums[i];
         }
         return total-sum;
+    }
+    
+    // Bit method.
+    private int solution2(int[] nums) {
+        // key: initiating as length for the edge case.
+        int missed = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missed = missed ^ nums[i] ^ i;
+        }
+        return missed;
     }
 }

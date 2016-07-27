5e33f5c66727665148edf74c37ae272b6add92a8
Cady Wang <cadywsq@gmail.com>
2016-07-26 21:06:29 -0400

    239. Sliding Window Maximum@Cady

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -4,7 +4,11 @@ public class Solution {
         if (n == 0) {
             return nums;
         }
-        
+        return heapSolution(nums, k, n);
+    }
+    
+    /**Solution1: Deque, amortized O(N) time*/
+    public int[] dequeSolution(int[] nums, int k, int n) {
         int[] result = new int[n-k+1];
         // store index of element
         Deque<Integer> queue = new ArrayDeque<>();
@@ -23,6 +27,24 @@ public class Solution {
             if (i-k+1 >= 0) {
                 result[i-k+1] = nums[queue.peek()];
             }
+        }
+        return result;
+    }
+    
+    /**Solution2: maxHeap, O(Nlogk) time*/
+    public int[] heapSolution(int[] nums, int k, int n) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> b-a);
+        int[] result = new int[n-k+1];
+        
+        for (int i = 0; i < k; i++) {
+            queue.offer(nums[i]);
+        }
+        result[0] = queue.peek();   
+        
+        for (int i = k; i < n; i++) {
+            queue.remove(nums[i-k]);
+            queue.offer(nums[i]);
+            result[i-k+1] = queue.peek();
         }
         return result;
     }

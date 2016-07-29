ecf47a64fc6da1e979152fbcd79491ef4bdbfdd7
Cady Wang <cadywsq@gmail.com>
2016-07-28 22:35:54 -0400

    140. Word Break II@Cady

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,52 +1,46 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
         boolean[] canBreak = canBreak(s, wordDict);
-        List<String> result = wordBreak(s, wordDict, s.length(), canBreak);
+        List<String> result = wordBreakHelper(s, wordDict, canBreak, s.length());
         return result;
     }
     
-    private List<String> wordBreak(String s, Set<String> wordDict, int index, boolean[] canBreak) {
-        List<String> list = new ArrayList<>();
-        
-        //Return an empty list if cannot break
+    private List<String> wordBreakHelper(String s, Set<String> wordDict, boolean[] canBreak, int index) {
+        List<String> result = new ArrayList<>();
         if (!canBreak[index]) {
-            return list;
+            return result;
         }
-        //Return a list containing an empty string if reach end
         if (index == 0) {
-            list.add("");
-            return list;
+            result.add("");
+            return result;
         }
         for (int i = index-1; i >= 0; i--) {
             String suffix = s.substring(i, index);
             if (wordDict.contains(suffix)) {
-                for (String prefix : wordBreak(s, wordDict, i, canBreak)) {
-                    String space = null;
-                    if (prefix.length() == 0) {
+                for (String prefix : wordBreakHelper(s, wordDict, canBreak, i)) {
+                    String space;
+                    if (prefix.equals("")) {
                         space = "";
                     } else {
                         space = " ";
                     }
-                    list.add(prefix + space + suffix);
+                    result.add(prefix + space + suffix);
                 }
             }
         }
-        return list;
+        return result;
     }
     
     private boolean[] canBreak(String s, Set<String> wordDict) {
-        int len = s.length();
         // canBreak[i] represents that s.substring(0,i) is breakable, i excluded.
-        boolean[] canBreak = new boolean[len+1];
+        boolean[] canBreak = new boolean[s.length()+1];
         canBreak[0] = true;
         
         for (int i = 1; i <= s.length(); i++) {
             for (String word : wordDict) {
                 int j = i - word.length();
-                if (j < 0) continue;
-                if (canBreak[j] && s.substring(j, i).equals(word)) {
+                if (j >= 0 && canBreak[j] && s.substring(j, i).equals(word)) {
                     canBreak[i] = true;
-                    break;
                 }
             }
         }
